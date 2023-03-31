52f81a564888e80c6306aa46ca38de1f367fe390
Duncan Krey <duncankrey@Duncans-Air.fios-router.home>
2023-03-31 15:09:05 -0700

    Ensuring up to date

--- website/src/components/CategoryCard.js
+++ website/src/components/CategoryCard.js
@@ -2,7 +2,13 @@ import "./CategoryCard.css";
 
 function CategoryCard() {
     return (
-        <div className='card-container'>Category Card Placeholder</div>
+        <div className='card-container'>
+            <img />
+            <div>
+                <div>#Process</div>
+                <div>10 Uploads</div>
+            </div>
+        </div>
     );
 }
 
