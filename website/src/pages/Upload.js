7f1ac5f195cf631c9592c2dd9d34182f14d0c2b6
vvnhng <vvnhng@uw.edu>
2023-03-31 15:30:59 -0700

    upload js

---
+++ website/src/pages/Upload.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+function Upload() {
+  return (
+    <div className='upload-container'>
+      <NavBar />
+      <CategoryCard />
+      <CategoryCard />
+      <CategoryCard />
+    </div>
+  )
+}
+
+export default Upload;
