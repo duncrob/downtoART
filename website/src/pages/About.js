919d2ae288151252bb69fc769c26042579fc2975
vvnhng <vvnhng@uw.edu>
2023-05-03 15:52:37 -0700

    about page

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -5,9 +5,23 @@ function About() {
     <div className='about-container'>
       <div className="about-content">
         <div className="about-header">About HEART</div>
-        <div className="about-text">Overview: Create a simple, single website that provides an overview of your project, your target stakeholders and users, a summary of what you did (your actions) and what benefit your project or solution provides. Students submit their website URL on the Capstone web form.</div>
-        <div className="about-text">Stakeholders and Users: UW Students</div>
-        <div className="about-text">Summary: </div>
+        <div className="about-text">Introducing HEART - the solution to bridging the gap between “non-creatives” and “creatives” and empowering inclusion among the UW community.</div>
+        <div className="about-text">HEART is a platform that lowers the barrier of entry for anyone who wants to post and express their creative endeavors. Whether you're a seasoned artist or a beginner trying to explore your creativity, HEART welcomes you to share your work with the world.
+        <p></p>
+        But HEART is more than just an art gallery - it's a community of like-minded individuals who are passionate about creativity and innovation. By sharing your work on HEART, you can connect with others who share your interests.
+        </div>
+        <p></p>
+        <div className="about-header">About the Project</div>
+        <div className="about-text">
+        Stakeholders and Users: UW Students and Faculty
+        <p></p>
+        How We Developed This Website:
+        <li>Researching, testing, and planning in Miro</li>
+        <li>Wireframing and prototyping in Figma</li>
+        <li>Coding in HTML, CSS, and JavaScript using React framework</li>
+        <li>Data stored in Firebase</li>
+        </div>
+        <div className="about-text">Created by: Vivian Hung, Duncan Krey, Best Watanapalin, Cameron Lee, and Hermon Rusom</div>
       </div>
     </div>
   )
