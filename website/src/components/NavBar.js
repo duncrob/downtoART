1f8e2189f929e7b407d5cba97b9e1c22bd0a9730
vvnhng <vvnhng@uw.edu>
2023-05-11 20:51:21 -0700

    navbar2 changes

--- website/src/components/NavBar.js
+++ website/src/components/NavBar.js
@@ -11,12 +11,17 @@ function NavBar() {
             <div className="title-text">
                 <img className="logo" src="../img/HEART.png" alt="Heart Logo with box" onClick={() => navigate("/home")}/>
             </div>
+            <div className="navbar-btn">
+                <div className="home-btn">HOME</div>
+                <div className="my-gallery-btn">MY GALLERY</div>
+            </div>
             {/* <div className='nav-buttons'>
                 <div className='search-btn'>
                     <FontAwesomeIcon icon={faSearch} size='xl' />
                     <input type="text" placeholder="" className="search-bar"/>
                 </div>
             </div> */}
+
         </div>
     );
 }
